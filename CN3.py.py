9bbbdf8c3bef3cbf03facf7dd606c7d705807524
pallavi marbe <pallavimarbe10@gmail.com>
2023-11-29 21:12:02 +0530

    c

---
+++ CN3.py.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n=int(input(
+d=2
+flag=False
+while d<n:
+    if(n%d==0):
+        flag=True
+    d=d+1
+if flag:
+    print("not prime")
+else:
+    print("prime")
+
+
+# #nested loop
+
+# In[ ]:
+
+
+n=int(input())
+k=2
+while k<=n:
+    d=2
+    flag=False
+    while d<k: 
+        if(k%d==0):
+            flag=True      #op:10
+        d=d+1                   3
+if not (falg):                  5
+    print(k)                    7
+k=k+1
+
