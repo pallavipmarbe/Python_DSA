098ab4d5c18bb4958aeb599f5336bca9c597d6fe
pallavi marbe <pallavimarbe10@gmail.com>
2024-01-01 23:47:40 +0530

    c

--- function.py.py
+++ function.py.py
@@ -185,9 +185,11 @@ print(sum(3,4))
 
 
 #all non default arguments should be before argument with default values
-def (a=0,b,c):
-    return a+b+c
-f(1,2)
+def f(a=0, b=0, c=0):
+    return a + b + c
+
+result = f(1, 2)
+print(result)
 
 
 # In[69]:
