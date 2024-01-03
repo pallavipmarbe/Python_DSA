8bc6b78bd87585d1243b53926ce348c1eef62d35
pallavi marbe <pallavimarbe10@gmail.com>
2024-01-03 23:34:20 +0530

    c

---
+++ lists.py.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# create a list
+
+# In[2]:
+
+
+li=[1,2,3]
+type(li)
+
+
+# In[4]:
+
+
+li=[1,2,"parikh",3.4]
+li
+
+
+# access and change elements in list
+
+# In[6]:
+
+
+li[0]
+
+
+# In[8]:
+
+
+li[2]
+
+
+# In[10]:
+
+
+li[3]
+
+
+# In[12]:
+
+
+li[4]
+
+
+# In[15]:
+
+
+li[0]=5
+li
+
+
+# In[17]:
+
+
+li[3]="rohan"
+li
+
+
+# In[19]:
+
+
+li[4]=9
+
+
+# slicing of a list
+
+# In[21]:
+
+
+li[1:3]
+
+
+# In[23]:
+
+
+li
+
+
+# In[25]:
+
+
+li[1:]
+
+
+# In[27]:
+
+
+li[:]
+
+
+# In[29]:
+
+
+li[1:10]
+
